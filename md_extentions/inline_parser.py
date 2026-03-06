8077fa4538f56ab45040c37e783bb80934eb241b
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2026-03-07 08:54:35 +0900

    inline_parser: インラインコード追加
    - md_extentions/inline_parser.py
    - `code` -> <code>code</code>

--- md_extentions/inline_parser.py
+++ md_extentions/inline_parser.py
@@ -14,8 +14,17 @@ r"""
             - `?`: 
 """
 RE_INLINE = re.compile(r'@@\{([^}]*)\}(.+?)@@', re.S)
+RE_CODE = re.compile(r'`([^`]+)`')
 
 def convert_inline(text: str) -> str:
+
+    codes = []
+
+    def code_repl(m):
+        codes.append(m.group(1))
+        return f"\uE000{len(codes) - 1}\uE000"
+    
+    text = RE_CODE.sub(code_repl, text)
 
     def repl(match):
         """ 対象文字列をクラス属性付きspanで囲む
@@ -36,4 +45,11 @@ def convert_inline(text: str) -> str:
         #   -> <span class="f-sz-xl fst-bold fc-red">foobar2000</span>
         return f'<span class="{class_attr}">{content}</span>'
     
-    return RE_INLINE.sub(repl, text)
+    text = RE_INLINE.sub(repl, text)
+
+    # 退避していたインラインコード文字列を復元
+    for i, c in enumerate(codes):
+        text = text.replace(f"\uE000{i}\uE000", f"<code>{c}</code>")
+    # 変換後の文字列を返却
+    return text
+    
