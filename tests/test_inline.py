1a59ef4a2ff3777994d68cd98ea1e27dd9db34fd
seo tomohiro <seoo_japan_keirinoh@yahoo.co.jp>
2026-03-08 15:28:21 +0900

    test_inline: インライン書式用のテストを追加
    - `*`, `_`エスケープのテスト

---
+++ tests/test_inline.py
@@ -0,0 +1,19 @@
+import pytest
+
+from md_extensions.parsers.inline_parser import convert_inline
+
+@pytest.mark.parametrize(
+    "src, expected", 
+    [
+        ("This is *italic*.", "This is <em>italic</em>."), 
+        ("This is _italic_.", "This is <em>italic</em>."), 
+        (r"This is not \*italic\*.", "This is not *italic*."), 
+        (r"This is not \_italic\_.", "This is not _italic_."), 
+        (r"*italic \* inside*", "<em>italic * inside</em>"), 
+        (r"**bold *italic* bold**", "<strong>bold <em>italic</em> bold</strong>"), 
+        (r"**bold _italic_ bold**", "<strong>bold <em>italic</em> bold</strong>"), 
+        (r"Shift\_JIS is a Shift\_JIS.", "Shift_JIS is a Shift_JIS."), 
+    ]
+)
+def test_inline(src, expected):
+    assert convert_inline(src) == expected
