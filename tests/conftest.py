3b9544c530a324aed382ca9bb657dbb624390df2
seo tomohiro <seoo_japan_keirinoh@yahoo.co.jp>
2026-03-28 17:00:31 +0900

    conftest: pytestの実行方法のメモを追加

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,14 @@
 import sys
 from pathlib import Path
 
+"""
+## テストコードの実行
+- プロジェクトルートをカレントディレクトリにする
+- `pytest tests\test_inline.py`コマンドを叩く
+## 技術的な解説
+- `conftest.py`でルートディレクトリのパスを`PYTHONPATH`に追加
+    - `sys.path.append(str(Path(__file__).resolve().parents[1]))`
+    - 〝このファイル〟があるディレクトリの上の上のディレクトリ
+"""
+
 sys.path.append(str(Path(__file__).resolve().parents[1]))
